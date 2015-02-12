97f11e1289e223689a2abdef14747dcaee1227ee
Laura Dodd <ldodd@cern.ch>
2015-02-12 19:05:39 +0100

    Adding RunNumberFactory

---
+++ plugins/RunNumberFactory.hh
@@ -0,0 +1,16 @@
+#ifndef RunNumberFactory_hh
+#define RunNumberFactory_hh
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+class RunNumberFactory {
+
+public:
+
+  int RunSummary();
+};
+
+#endif
